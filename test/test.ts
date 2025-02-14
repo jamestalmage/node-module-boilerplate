01ba8f435aa34010f19205b34b3fe73a3489e9ad
James Talmage <james@talmage.io>
2025-02-14 01:10:53 -0500

    tweaks

--- test/test.ts
+++ test/test.ts
@@ -1,9 +1,9 @@
 import test from 'ava';
-import unicornFun from '../src';
+import unicornFun from '../src/index.js';
 
 test('main', t => {
 	t.throws(() => {
-		unicornFun(123);
+		unicornFun(123 as unknown as string);
 	}, {
 		instanceOf: TypeError,
 		message: 'Expected a string, got number',
